5d1577b28f394018031b94c867159fddfa9f3288
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-11 17:15:05 +0200

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Razor.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int Avaibility { get; set; }
+        public double Price { get; set; }
+
+        public Book(){}
+        public Book(string Title, string Author, string ISBN, int Avaibility, double Price)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.ISBN = ISBN;
+            this.Avaibility = Avaibility;
+            this.Price = Price;
+        }
+    }
+}
