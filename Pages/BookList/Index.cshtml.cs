9de1a7d3431e8a518a3c3a021a657e4497a3e48d
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-17 17:59:47 +0200

    index pages

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -2,17 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookList_Razor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookList_Razor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public string SomeData { get; set; }
-        public void OnGet()
+        private AppDbContext context;
+        public IEnumerable<Book> Books { get; set; }
+        
+        public IndexModel(AppDbContext Context)
         {
-            SomeData = "Hello from page model!";
+            this.context = Context;
+        }
+        
+        public async Task OnGet()
+        {
+            Books = await context.Books.ToListAsync();
         }
     }
 }
